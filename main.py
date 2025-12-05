2a1663b54d7331255e56da491aedca8728362be8
James <connojt3@gmail.com>
2025-12-04 20:21:11 -0500

    Default setting for VBFM integration

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from eventbus import EventBus
 from gui.app import DSPGui
-from hardware.mock import MockHW   # swap to: from hardware.rpi import RPiHW
+from hardware.rpi import RPiHW   # swap to: from hardware.rpi import RPiHW
 
 if __name__ == "__main__":
     bus = EventBus()
-    hw = MockHW()  # replace with RPiHW() when ready
+    hw = RPiHW()  # replace with RPiHW() when ready
     app = DSPGui(hw, bus)  
     app.mainloop()
