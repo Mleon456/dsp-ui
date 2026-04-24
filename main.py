16e671c45a24f0c8aa7041173fbc2a55e8429a04
James <connojt3@gmail.com>
2026-04-23 20:20:51 -0400

    Swapped to HW

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from eventbus import EventBus
 from gui.app import DSPGui
-#from hardware.rpi import RPiHW
-from hardware.mock import MockHW  # swap to: from hardware.rpi import RPiHW
+from hardware.rpi import RPiHW
+#from hardware.mock import MockHW  # swap to: from hardware.rpi import RPiHW
 
 if __name__ == "__main__":
     bus = EventBus()
-    hw = MockHW()  # RPiHW or MockHW
+    hw = RPiHW()  # RPiHW or MockHW
     app = DSPGui(hw, bus)  
     app.mainloop()
